5c1b572d7c11659e8a218daf62401368f24d941d
Shreyasi Chakraborty <59410716+shreyasichakraborty@users.noreply.github.com>
2020-06-23 22:43:39 +0530

    Sub, Prod, Div added

--- test.java
+++ test.java
@@ -9,5 +9,7 @@ System.out.println("Enter second number");
 int b = s.nextInt();
 System.out.println("Addition: "+(a+b));
 System.out.println("Subtraction: "+(a-b));
+System.out.println("Product: "+(a*b));
+System.out.println("Division: "+(a/b));
 }
 }
