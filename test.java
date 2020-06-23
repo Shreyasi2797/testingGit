ab4dcd809b330776ba8a7c84125840a5d9aff9fd
Shreyasi Chakraborty <59410716+shreyasichakraborty@users.noreply.github.com>
2020-06-23 22:23:54 +0530

    second commit - prints addition of 2 nos

--- test.java
+++ test.java
@@ -1,5 +1,10 @@
+import java.util.*;
 public class test{
 public static void main(String args[]){
 System.out.println("Hello world");
+Scanner s = new Scanner(System.in);
+int a = s.nextInt();
+int b = s.nextInt();
+System.out.println("Addition: "+(a+b));
 }
 }
