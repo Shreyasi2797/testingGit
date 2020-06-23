71112a3b42d78c6477a72d8da2411ce3abbf3461
Shreyasi Chakraborty <59410716+shreyasichakraborty@users.noreply.github.com>
2020-06-23 22:34:19 +0530

    Addition of 2 nos

--- test.java
+++ test.java
@@ -3,8 +3,10 @@ public class test{
 public static void main(String args[]){
 System.out.println("Hello world");
 Scanner s = new Scanner(System.in);
+System.out.println("Enter first number");
 int a = s.nextInt();
+System.out.println("Enter second number");
 int b = s.nextInt();
-System.out.println("Addition: "+(a+b));
+System.out.println("Addition:"+(a+b));
 }
 }
